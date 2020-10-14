e5433616b530c271d2cc9e3a0e83f5e4143644ae
damenor <damenor.develop@gmail.com>
2020-10-14 09:35:09 +0200

    arrays and numbers completed

---
+++ test/index.js
@@ -0,0 +1,15 @@
+describe('Sumas', () => {
+  
+  // Suma de + con + tiene que ser +
+  it('Suma de + con + tiene que ser +', () => {
+    console.log(2 + 2)
+    console.log(4)
+  })
+  
+  // Suma de + mayor con - menor tiene que ser + 
+  it('Suma de + mayor con - menor tiene que ser + ', () => {
+    console.log(2 + (-1))
+    console.log(1)
+  })
+
+})
